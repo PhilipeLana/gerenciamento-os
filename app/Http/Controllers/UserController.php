f6a550ea08593f5306e58c605ba26f82418070e6
PhilipeLana <philipealeflana@gmail.com>
2026-02-26 17:53:36 -0300

    feito estrura base de usuario e niveis de acesso

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    //lista todos os usuarios cadastrados para o admin( temos que implementar uma tela sozinha para isto)
+    public function index()
+    {
+        $users = User::all();
+        return view('users.index', compact('users'));
+    }
+
+    //processsa o novo cadastro
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'perfil' => 'required|in:admin,comum',
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'perfil' => $request->perfil,
+        ]);
+
+        return redirect()->back()->with('success', 'Usuário cadastrado com sucesso!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/usuarios', [UserController::class, 'index']);
+
+Route::post('/usuarios/store', [UserController::class, 'store'])->name('users.store');
